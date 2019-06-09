2c263bc3365cf54a34ff4eac2013613c304ff5e1
Aaron Robb <airrobb@gmail.com>
2019-06-09 15:42:17 -0400

    Update title and fix CSStevez

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Head from "next/head";
+
 import content from "../content/home.md";
 
 import "../styles/index.css";
@@ -7,6 +9,9 @@ export default class Home extends Component {
   render() {
     return (
       <div className="page">
+        <Head>
+          <title>Aaron's Party</title>
+        </Head>
         <h1 className="page-title">Come get it</h1>
       </div>
     );
