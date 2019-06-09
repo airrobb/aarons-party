69ecacdb4ee1d54a5380a4fb6ee4f62550e198d4
Aaron Robb <airrobb@gmail.com>
2019-06-09 15:35:16 -0400

    Come get it

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-module.exports = {
-  webpack: cfg => {
-    cfg.module.rules.push({
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
+  webpack(config, options) {
+    config.module.rules.push({
       test: /\.md$/,
       use: "frontmatter-markdown-loader"
     });
-    return cfg;
+    return config;
   }
-};
+});
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import content from "../content/home.md";
 
+import "../styles/index.css";
+
 export default class Home extends Component {
   render() {
-    return <h1>Come get it</h1>;
+    return (
+      <div className="page">
+        <h1 className="page-title">Come get it</h1>
+      </div>
+    );
   }
 }
