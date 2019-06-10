6275b3724fc1d22488b901483a7a19c54e26fecf
Aaron Robb <airrobb@gmail.com>
2019-06-09 22:03:58 -0400

    Do things

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import Head from "next/head";
+import styled from "styled-components";
 
 import "../styles/index.css";
 import { tasteTheRainbow } from "../utility/fun";
 
 const rainbow = tasteTheRainbow();
 
-export default function Home() {
-  const [hovering, setHover] = React.useState(false);
+const Tasty = styled.a`
+  font-size: 10rem;
+  text-align: center;
+  transition: text-shadow 0.5s;
+  &:hover {
+    text-shadow: ${rainbow};
+  }
 
+  @media (max-width: 768px) {
+    font-size: 13vw;
+  }
+`;
+
+export default function Home() {
   return (
     <div className="page">
       <Head>
         <title>Aaron's Party</title>
       </Head>
-      <h1
-        className="page-title"
-        onMouseOver={() => setHover(true)}
-        onMouseOut={() => setHover(false)}
-        style={{ textShadow: hovering ? rainbow : null }}
-      >
-        Come Get It
-      </h1>
+      <Tasty>Come Get It</Tasty>
     </div>
   );
 }
